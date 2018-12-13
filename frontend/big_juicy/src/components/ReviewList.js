428eef62ba0c4ed89d4bfadd8b1e603f1415d96f
Iain Floyd <iain.floyd@live.com>
2018-12-13 17:41:06 +0000

    added reviewList code

---
+++ frontend/big_juicy/src/components/ReviewList.js
@@ -0,0 +1,25 @@
+import React, {Component, Fragment} from 'react';
+import Review from './Review.js';
+
+const ReviewList = (props) => {
+  const arrayOfReviews = props.reviews.map((rev) => {
+    return (
+      <Review
+        pub={rev.pub}
+        user={rev.user}
+        image={rev.image}
+        overall={rev.overall}
+        price={rev.price}
+        text={rev.text}
+      />
+    )
+  })
+
+  return (
+    <div className="review-thingy">
+      {arrayOfReviews}
+    </div>
+  )
+}
+
+export default ReviewList;
