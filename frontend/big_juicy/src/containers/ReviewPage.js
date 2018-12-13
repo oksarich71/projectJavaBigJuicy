1629947ae45501e9785be45a3bc355ec1d7b7cac
Iain Floyd <iain.floyd@live.com>
2018-12-13 16:52:11 +0000

    adds empty containers

---
+++ frontend/big_juicy/src/containers/ReviewPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const ReviewPage = (props) => {
+  return (
+    <div>
+      <h2>hi this is the reviewpage you're seeing</h2>
+      <FormsListContainer/>
+    </div>
+  )
+}
+
+export default ReviewPage;
+
+// <FormsListContainer>
+// <ReviewListContainer>
